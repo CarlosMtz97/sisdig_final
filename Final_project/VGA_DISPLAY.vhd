75dacda64abcbb1f8f0c1c8cd6e8a576e0a7508d
CarlosMtz97 <A00820293@itesm.mx>
2020-12-07 11:02:28 -0600

    movimiento

--- Final_project/VGA_DISPLAY.vhd
+++ Final_project/VGA_DISPLAY.vhd
@@ -22,6 +22,8 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith.all;
+
 
 entity VGA_DISPLAY is
   generic (
@@ -30,17 +32,17 @@ entity VGA_DISPLAY is
 	 PosCol    : natural := 100;    -- Top left corner column position where image will be drawn
 	 PosRow    : natural := 100);   -- Top left corner row position where image will be drawn
   port (
-    Clk : in  STD_LOGIC;
-	 Xin : in  STD_LOGIC_VECTOR(9 downto 0); -- Column screen coordinate
-	 Yin : in  STD_LOGIC_VECTOR(9 downto 0); -- Row screen coordinate
-	 En  : in  STD_LOGIC;                    -- When '1', pixels can be drawn 
+    Clk    : in  STD_LOGIC;
+	 Xin    : in  STD_LOGIC_VECTOR(9 downto 0); -- Column screen coordinate
+	 Yin    : in  STD_LOGIC_VECTOR(9 downto 0); -- Row screen coordinate
+	 En     : in  STD_LOGIC;                    -- When '1', pixels can be drawn 
 	 rightB : in  STD_LOGIC;
 	 leftB  : in  STD_LOGIC;
 	 upB    : in  STD_LOGIC;
 	 downB  : in  STD_LOGIC;
-	 R   : out STD_LOGIC_VECTOR(2 downto 0); -- 3-bit Red channel
-	 G   : out STD_LOGIC_VECTOR(2 downto 0); -- 3-bit Green channel
-	 B   : out STD_LOGIC_VECTOR(1 downto 0));-- 2-bit Blue channel
+	 R      : out STD_LOGIC_VECTOR(2 downto 0); -- 3-bit Red channel
+	 G      : out STD_LOGIC_VECTOR(2 downto 0); -- 3-bit Green channel
+	 B      : out STD_LOGIC_VECTOR(1 downto 0));-- 2-bit Blue channel
 end VGA_DISPLAY;
 
 architecture Behavioral of VGA_DISPLAY is
@@ -55,15 +57,34 @@ architecture Behavioral of VGA_DISPLAY is
   -- Embedded signal to group the colors into 1-byte
   -- The colors will be as follows:
   --  R2 R1 R0 G2 G1 G0 B1 B0
-  signal Color :  STD_LOGIC_VECTOR(7 downto 0);
+  signal   Color      : STD_LOGIC_VECTOR(7 downto 0);
+  constant COLOR_BG   : STD_LOGIC_VECTOR ( 7 downto 0) := "01001000";
+  constant COLOR_BALL : STD_LOGIC_VECTOR ( 7 downto 0) := "11011101";
+  constant COLOR_NULL : STD_LOGIC_VECTOR ( 7 downto 0) := "00000000";
   
   -- ROM memory Address and Data Bus
   signal Address: STD_LOGIC_VECTOR(11 downto 0);
   signal Data:    STD_LOGIC_VECTOR( 7 downto 0);
 
   signal Location : natural range 0 to ((ImageCols * ImageRows) - 1);  
+  
+  -- sizes
+  constant L_BOUND    : natural                        :=  60;
+  constant R_BOUND    : natural                        := 580;
+  constant U_BOUND    : natural                        :=  40;
+  constant D_BOUND    : natural                        := 440;
+  
+  -- Embedded signals
+  signal rgb_now     : STD_LOGIC_VECTOR ( 7 downto 0);
+  signal clk_frame   : STD_LOGIC;
+  
+   -- velocities
+  constant cookie_v     : natural                        :=   1;
 
 begin
+  clk_frame <= '1' when (Xin = 0 and Yin = 0) else
+               '0';
+					
   C01 : ROM_1
   port map (
      CLKA  => Clk,
@@ -86,6 +107,48 @@ begin
 		  Color <= "00000000"; -- Black
 	   end if;
 	 end if;
+  end process;
+ 
+   process (Clk,Xin,Yin)
+    variable cookie_x      : STD_LOGIC_VECTOR (10 downto 0)  := "00101000000"; --320
+    variable cookie_y      : STD_LOGIC_VECTOR (10 downto 0)  := "00011110000"; --240
+  begin
+    if (rising_edge(clk)) then
+	   if (clk_frame = '1') then
+		  -- ball control
+		  if (rightB = '1' and cookie_x < CONV_STD_LOGIC_VECTOR(R_BOUND,11)) then   
+		    cookie_x := cookie_x + cookie_v;
+		  end if;
+		  if (leftB = '1' and cookie_x > CONV_STD_LOGIC_VECTOR(L_BOUND,11)) then
+		    cookie_x := cookie_x - cookie_v;
+		  end if;
+		  if (downB = '1' and cookie_y < CONV_STD_LOGIC_VECTOR(D_BOUND,11)) then
+		    cookie_y := cookie_y + cookie_v;
+		  end if;
+		  if (upB = '1' and cookie_y > CONV_STD_LOGIC_VECTOR(U_BOUND,11)) then
+		    cookie_y := cookie_y - cookie_v;
+		  end if;
+			
+        -- reset, re-center ball
+--        if (reset = '1') then
+--          cookie_x := CONV_STD_LOGIC_VECTOR(320,11);
+--          cookie_y := CONV_STD_LOGIC_VECTOR(240,11);
+--        end if;		 		 
+		end if;
+	 end if;
+
+	-- drawing section ---
+	
+	-- draw background
+   if (Xin >= 0 and Yin >= 0 and Xin < 640 and Yin < 480) then
+	   rgb_now <= COLOR_BG;
+			
+   else
+	   -- outside of the display area, fill black
+		rgb_now <= COLOR_NULL;
+		
+	end if;		
+     
   end process;
   
   -- Send individual color to their channel
--- Final_project/pa.fromNetlist.tcl
+++ Final_project/pa.fromNetlist.tcl
@@ -1,10 +1,11 @@
 
 # PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name Final_project -dir "D:/Semestre_9/Sistemas_Digitales_Avanzados/sisdig_final/Final_project/planAhead_run_2" -part xc6slx16csg324-3
+create_project -name Final_project -dir "D:/Semestre_9/Sistemas_Digitales_Avanzados/sisdig_final/Final_project/planAhead_run_3" -part xc6slx16csg324-3
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
 set_property edif_top_file "D:/Semestre_9/Sistemas_Digitales_Avanzados/sisdig_final/Final_project/Top.ngc" [ get_property srcset [ current_run ] ]
-add_files -norecurse { {D:/Semestre_9/Sistemas_Digitales_Avanzados/sisdig_final/Final_project} }
+add_files -norecurse { {D:/Semestre_9/Sistemas_Digitales_Avanzados/sisdig_final/Final_project} {ipcore_dir} }
+add_files [list {ipcore_dir/ROM_1.ncf}] -fileset [get_property constrset [current_run]]
 set_param project.pinAheadLayout  yes
 set_property target_constrs_file "Top.ucf" [current_fileset -constrset]
 add_files [list {Top.ucf}] -fileset [get_property constrset [current_run]]
